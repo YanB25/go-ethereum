cadd5c20de22f2e30effe1319d4c13b42809b0c9
BensonYan <yanb25@mail2.sysu.edu.cn>
2019-04-02 19:46:56 +0800

    Update json.go

--- common/hexutil/json.go
+++ common/hexutil/json.go
@@ -358,9 +358,10 @@ func checkNumberText(input []byte) (raw []byte, err error) {
 	if len(input) == 0 {
 		return nil, ErrEmptyNumber
 	}
-	if len(input) > 1 && input[0] == '0' {
-		return nil, ErrLeadingZero
-	}
+	// no checking leading zero.
+	// if len(input) > 1 && input[0] == '0' {
+	// 	return nil, ErrLeadingZero
+	// }
 	return input, nil
 }
 
